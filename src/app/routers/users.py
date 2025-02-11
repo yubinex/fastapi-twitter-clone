39bae1a7b5e69cd89c86b9fb27994ff3879579c3
yubinex <janhauck99@gmail.com>
2025-02-11 04:07:49 +0100

    add dev endpoint to retrieve the user currently logged in

--- src/app/routers/users.py
+++ src/app/routers/users.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session
 
+from app.auth.auth import get_current_user
 from app.crud.users import create_user, login_user
-from app.db_and_models.models import UserModel
+from app.db_and_models.models import User, UserModel
 from app.db_and_models.session import get_session
 
 router = APIRouter(tags=["Users"])
@@ -14,6 +15,11 @@ async def create_user_endpoint(
     user_model: UserModel, db: Session = Depends(get_session)
 ):
     return await create_user(user_model=user_model, db=db)
+
+
+@router.get("/users/me")
+async def read_current_user(current_user: User = Depends(get_current_user)):
+    return current_user
 
 
 @router.post("/login")
