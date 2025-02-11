7e9726a16ad7971b2890670e5954e1109e011a0d
yubinex <janhauck99@gmail.com>
2025-02-11 01:30:28 +0100

    implement basic login endpoint [WIP]

--- src/app/crud/users.py
+++ src/app/crud/users.py
@@ -1,4 +1,5 @@
 from fastapi import HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session, select
 
 from app.db_and_models.models import User, UserModel
@@ -16,3 +17,15 @@ async def create_user(user_model: UserModel, db: Session):
     db.commit()
     db.refresh(user)
     return {"success": f"user created with id {user.id}"}
+
+
+async def login_user(form_data: OAuth2PasswordRequestForm, db: Session):
+    existing_user = db.exec(
+        select(User).where(User.username == form_data.username)
+    ).first()
+    if not existing_user:
+        raise HTTPException(status_code=401, detail="authentification failed")
+
+    # TODO: validate password
+
+    return {"access_token": "fake_token", "token_type": "bearer"}
--- src/app/routers/users.py
+++ src/app/routers/users.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session
 
-from app.crud.users import create_user
+from app.crud.users import create_user, login_user
 from app.db_and_models.models import UserModel
 from app.db_and_models.session import get_session
 
@@ -13,3 +14,10 @@ async def create_user_endpoint(
     user_model: UserModel, db: Session = Depends(get_session)
 ):
     return await create_user(user_model=user_model, db=db)
+
+
+@router.post("/login")
+async def login_user_endpoint(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_session)
+):
+    return await login_user(form_data=form_data, db=db)
