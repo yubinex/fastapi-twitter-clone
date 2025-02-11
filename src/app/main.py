08c2bb559a18b72613a0e7a164c04a4fdea9f1c3
yubinex <janhauck99@gmail.com>
2025-02-11 23:58:11 +0100

    include post router

--- src/app/main.py
+++ src/app/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.db_and_models.session import create_db_and_tables, drop_tables
+from app.routers.posts import router as post_router
 from app.routers.users import router as user_router
 
 
@@ -10,9 +11,11 @@ from app.routers.users import router as user_router
 async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
+    # delete DB automatically (for better dev experience)
     drop_tables()
 
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(user_router)
+app.include_router(post_router)
