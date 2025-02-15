ee706ae536e0fd929a3e2b670aa00007f3f27f04
yubinex <janhauck99@gmail.com>
2025-02-15 23:50:36 +0100

    include follower router

--- src/app/main.py
+++ src/app/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.db_and_models.session import create_db_and_tables, drop_tables
+from app.routers.followers import router as follower_router
 from app.routers.likes import router as like_router
 from app.routers.posts import router as post_router
 from app.routers.users import router as user_router
@@ -18,6 +19,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.include_router(follower_router)
 app.include_router(like_router)
 app.include_router(post_router)
 app.include_router(user_router)
